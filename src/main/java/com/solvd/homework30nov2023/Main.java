d7049757ab2ae80794c1a88e9cbc47664bed4eaa
Atilio Boher <atilioboher@gmail.com>
2024-02-20 17:43:45 -0300

    Change project name

--- src/main/java/com/solvd/homework30nov2023/Main.java
+++ src/main/java/com/solvd/homework30nov2023/Main.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static com.solvd.homework30nov2023.homework.DesignPatternsHomework.designPatternsHomework;
+import static com.solvd.homework30nov2023.homework.MyBatisHomework.myBatisHomeTask;
+import static com.solvd.homework30nov2023.homework.XmlParsingHomework.xmlParsingHomeTask;
 
 public class Main {
 
